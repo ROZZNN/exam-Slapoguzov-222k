4cbf2328b99c4c1336cef04a299157607a20f322
ROZZ <157888462+ROZZNN@users.noreply.github.com>
2024-06-29 14:56:19 +0000

    update

--- exam-slapoguzov-222k/src/forma/forma.js
+++ exam-slapoguzov-222k/src/forma/forma.js
@@ -1,29 +1,17 @@
 import React from 'react';
 
 const formm = () => {
-    const handleSubmit = async (event) => {
-        event.preventDefault(); // Предотвращаем стандартное поведение формы
-    
-        const formData = new FormData(event.target); // Получаем данные формы
-        const data = Object.fromEntries(formData); // Преобразуем FormData в объект
-    
-        try {
-          alert('Дорожка успешно забронирована'); // Выводим сообщение
-        } catch (error) {
-          console.error('Ошибка при отправке данных:', error);
-        }
-    }
   return (
     <div class='form-reg'>
     <form action="/process-reservation" method="post" class='class-style'>
     <label for="name">Имя:</label>
-    <input type="text" id="name" name="name" required pattern="[А-Яа-я]+"/>
+    <input type="text" id="name" name="name" placeholder="Иван" required pattern="[А-Яа-я]+"/>
     
     <label for="phone">Телефон:</label>
-    <input type="tel" id="phone" name="phone" list="tel-list" placeholder="+7 (900) 123-45-67" required pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}"/>
+    <input type="tel" id="phone" name="phone" list="tel-list" placeholder="+7 (900) 123-45-67" required pattern="^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$"/>
     
     <label for="dateTimeStart">Дата и время начала брони:</label>
-    <input type="datetime-local" id="dateTimeStart" name="dateTimeStart" min="2022-04-17T00:00"/>
+    <input type="datetime-local" id="dateTimeStart" name="dateTimeStart" min="2022-04-18T00:00"/>
     
     <label for="duration">Длительность брони:</label>
     <select id="duration" name="duration" required>
@@ -35,7 +23,7 @@ const formm = () => {
     <input type="checkbox" id="agreeTerms" name="agreeTerms" required checked/>
     <label for="agreeTerms">Я согласен(-на) с условиями</label>
     
-    <button id="submitButton" type="submit">Забронировать</button>
+    <button>Забронировать</button>
     </form>
     </div>
     )
